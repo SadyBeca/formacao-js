5c7e688d9406929cf9e72759803bf236a748f9f5
SadyBeca <rebecasadyribeiro@gmail.com>
2023-08-15 12:05:18 -0300

    primeiro desafio dio

---
+++ desafio.js
@@ -0,0 +1,6 @@
+let precoDoComustivel = 5;
+let gastoMedioPorKm = 0.06;
+let distanciaDaViagem = 600;
+
+let gastoDaViagem = precoDoComustivel * gastoMedioPorKm * distanciaDaViagem; 
+console.log(gastoDaViagem);
