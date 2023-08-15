06e6cb2f0243f8bf87eee3c3ef95f1c38e6015a2
SadyBeca <rebecasadyribeiro@gmail.com>
2023-08-15 12:21:45 -0300

    corrigindo

--- desafio.js
+++ desafio.js
@@ -1,6 +1,16 @@
-let precoDoComustivel = 5;
-let gastoMedioPorKm = 0.06;
-let distanciaDaViagem = 600;
+// Faça um programa para calcular o valor de uma viagem.
+// Você terá 3 variáveis, são elas: 
+// 1- Preço do combustível;
+// 2- Gasto médio de combustível do carro por Km;
+// 3- Distância em Km da viagem;
+  
 
-let gastoDaViagem = precoDoComustivel * gastoMedioPorKm * distanciaDaViagem; 
-console.log(gastoDaViagem);
+
+let precoDoComustivelReaisPorLitro = 5.98;
+let gastoMedioKmPorLitro = 10;
+let distanciaDaViagemEmKm = 700;
+
+let totalDeLitros = distanciaDaViagemEmKm / gastoMedioKmPorLitro;
+
+let gastoDaViagem = precoDoComustivelReaisPorLitro * totalDeLitros; 
+console.log(gastoDaViagem.toFixed(2));
